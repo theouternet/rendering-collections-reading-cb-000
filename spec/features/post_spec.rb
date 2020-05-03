17da2d4101ef58e31f027eb95ec51cf1b246d602
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-05-03 09:21:09 +0000

    Done.

--- spec/features/post_spec.rb
+++ spec/features/post_spec.rb
@@ -6,8 +6,7 @@ describe 'navigate' do
     @post = Post.create(title: "A Time To Kill", description: "A Time to Kill is a 1989 legal suspense thriller by John Grisham. It was Grisham's first novel. The novel was rejected by many publishers before Wynwood Press (located in New York) eventually gave it a modest 5,000-copy printing. After The Firm, The Pelican Brief, and The Client became bestsellers, interest in A Time to Kill grew; the book was republished by Doubleday in hardcover and, later, by Dell Publishing in paperback, and itself became a bestseller. This made Grisham extremely popular among readers.", author_id: 1)
   end
 
-
-  it 'shows the title on the show page in a h1 tag' do
+ it 'shows the title on the show page in a h1 tag' do
     visit post_path(@post)
     expect(page).to have_css("h1", text: "A Time To Kill")
   end
@@ -40,7 +39,7 @@ describe 'form' do
     expect(page).to have_content("My post title")
   end
 
-  it 'shows an edit form that submits content and redirects and prints out params' do
+ it 'shows an edit form that submits content and redirects and prints out params' do
 
     visit edit_post_path(@post)
 
